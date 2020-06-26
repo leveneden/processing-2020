b907ac3dc986271917e4a44c4cb3fe2e282def5e
Daniel Chirinos <leveneden@hotmail.com>
2020-06-26 02:07:12 -0300

    Checkpoint, will keep filling the gaps tomorrow

--- src/main/java/k_means_image_processor/KMeansImageProcessorApplication.java
+++ src/main/java/k_means_image_processor/KMeansImageProcessorApplication.java
@@ -1,6 +1,10 @@
 package k_means_image_processor;
 
 import processing.core.PApplet;
+import processing.core.PVector;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class KMeansImageProcessorApplication extends PApplet {
 
@@ -11,6 +15,38 @@ public class KMeansImageProcessorApplication extends PApplet {
 
     @Override
     public void setup() {
+
+        List<PVector> vectors = new ArrayList<>();
+
+        vectors.add(new PVector(14, 5, 19));
+        vectors.add(new PVector(6, 17, 4));
+        vectors.add(new PVector(2, 8, 11));
+        vectors.add(new PVector(9, 16, 3));
+
+        int timesDivided = 1;
+        PVector average = new PVector();
+
+        for (PVector vector: vectors) {
+            PVector copy = vector.copy();
+            average.add(
+                    copy.sub(average).div(timesDivided)
+            );
+            timesDivided++;
+        }
+
+        for (PVector vector: vectors) {
+            debug(vector);
+        }
+
+        debug(average);
+
+    }
+
+    private void debug(PVector vector) {
+        System.out.println("x: " + vector.x);
+        System.out.println("y: " + vector.y);
+        System.out.println("z: " + vector.z);
+        System.out.println();
     }
 
     @Override
--- src/main/java/k_means_image_processor/model/KMeansImageProcessor.java
+++ src/main/java/k_means_image_processor/model/KMeansImageProcessor.java
@@ -2,8 +2,10 @@ package k_means_image_processor.model;
 
 import lombok.RequiredArgsConstructor;
 import processing.core.PApplet;
+import processing.core.PImage;
 import processing.core.PVector;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -12,10 +14,77 @@ public class KMeansImageProcessor {
     private PApplet processing;
 
     public KMeansResult process(int k, String imagePath) {
-        return null;
+        if (k<1) return new KMeansResult(new ArrayList<>(), 0);
+        return calculateMeans(k, getPixelsAsPVectors(imagePath));
     }
 
     private List<PVector> getPixelsAsPVectors(String imagePath) {
+        return parseToPVectors(getPixels(imagePath));
+    }
+
+    private int[] getPixels(String imagePath) {
+        PImage image = processing.loadImage(imagePath);
+        image.loadPixels();
+        return image.pixels;
+    }
+
+    private List<PVector> parseToPVectors(int[] pixels) {
+        List<PVector> vectors = new ArrayList<>();
+        for (int pixel: pixels)
+            vectors.add(new PVector(
+                processing.red(pixel),
+                processing.green(pixel),
+                processing.blue(pixel)
+            ));
+        return vectors;
+    }
+
+    private KMeansResult calculateMeans(int k, List<PVector> vectors) {
+        // get first centroids
+        // iterate
+
+        return null;
+    }
+
+    private List<PVector> deterministicallyGenerateCentroids(int amountOfCentroids, List<PVector> dataset) {
+        List <PVector> centroids = new ArrayList<>();
+
+        centroids.add(getAverage(dataset));
+
+        int remainingCentroidsToFind = amountOfCentroids - 1;
+
+        while (remainingCentroidsToFind > 0) {
+
+            centroids.add(getVectorInDatasetFarthestFromAllCentroids(dataset, centroids));
+
+            remainingCentroidsToFind--;
+        }
+
+
+
+        return null;
+    }
+
+    private PVector getAverage(List<PVector> vectors) {
+
+        int timesDivided = 1;
+        PVector average = new PVector();
+
+        for (PVector vector: vectors) {
+            PVector copy = vector.copy();
+            average.add(
+                    copy.sub(average).div(timesDivided)
+            );
+            timesDivided++;
+        }
+
+        return average;
+    }
+
+    private PVector getVectorInDatasetFarthestFromAllCentroids(List<PVector> dataset, List<PVector> centroids) {
+
+
+
         return null;
     }
 }
